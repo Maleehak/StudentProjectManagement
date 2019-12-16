2feb95eef7e8f2afa74ce7154fd31142374138af
aniqafatima <56432909+aniqafatima@users.noreply.github.com>
2019-12-16 14:32:48 +0500

    student crud

--- StudentProjectManagement/Student.aspx
+++ StudentProjectManagement/Student.aspx
@@ -1,18 +1,17 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Advisor.aspx.cs" Inherits="StudentProjectManagement.Advisor" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Student.aspx.cs" Inherits="StudentProjectManagement.Student" %>
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title>
-         University of Engineering and Techlogy ,Lahore
-    </title>
-    <meta charset="utf-8"/>
+    <title>University of Engineering and Technology,Lahore</title>
+     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link href="Style.css" rel="stylesheet"/>
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
 </head>
 <body>
-    <nav class="navbar navbar-expand-sm fixed-top" id="customColor">
+<nav class="navbar navbar-expand-sm fixed-top" id="customColor">
   <div class="container">
     <a class="navbar-brand" href="/">Student Project Management</a>
     <div class="navbar-collapse collapse">
@@ -32,7 +31,7 @@
                 <div class="col-md-3"></div>
                 <div class="col-md-6">
                     <div class="form-outer">
-                          <h1 class="text-center">Advisor</h1>
+                          <h1 class="text-center">Student</h1>
          <form id="form1" runat="server">
               <div class="input-container">
         <div>
@@ -48,11 +47,11 @@
                 </tr>
                  <tr>
                     <td>
-                        Rank:
+                        Registration No.:
                     </td>
                      
                     <td>
-                        <asp:TextBox ID="rank" runat="server" Width="300px" Height="50px" ></asp:TextBox>
+                        <asp:TextBox ID="regNum" runat="server" Width="300px" Height="50px" ></asp:TextBox>
                     </td>
                 </tr>
                   <tr>
@@ -68,11 +67,11 @@
                  
                   <tr>
                     <td>
-                        Designation:
+                        Degree:
                     </td>
                      
                     <td>
-                        <asp:TextBox ID="designation" runat="server" Width="300px" Height="50px"></asp:TextBox>
+                        <asp:TextBox ID="degree" runat="server" Width="300px" Height="50px"></asp:TextBox>
                     </td>
                 </tr>
                 <tr>
@@ -80,10 +79,10 @@
                      <td>
                      
                     </td>
-                    <td>  
-                        <asp:Button ID="Add" runat="server" Text="Add" BackColor="#009999" Height="40" Width="90px" ForeColor="White" OnClick="Add_Click" />
-                          <asp:Button ID="Update" runat="server" Text="Update" BackColor="#009999" Height="40" Width="90px" ForeColor="White" OnClick="Update_Click" />
-                         <asp:Button ID="Delete" runat="server" Text="Delete"  BackColor="#009999" Height="40" Width="90px" ForeColor="White" OnClick="Delete_Click" />
+                    <td>
+                        <asp:Button ID="Add" runat="server" Text="Add" BackColor="#009999" Height="40" Width="90px" ForeColor="White" OnClick="Add_Click"/>
+                        <asp:Button ID="Update" runat="server" Text="Update" BackColor="#009999" Height="40" Width="90px" ForeColor="White" OnClick="Update_Click" />
+                        <asp:Button ID="Delete" runat="server" Text="Delete" BackColor="#009999" ForeColor="White" Height="40" Width="90px" OnClick="Delete_Click" />
                     </td>
                     <td>
                        
@@ -101,4 +100,3 @@
    
 </body>
 </html>
-
---
+++ StudentProjectManagement/Student.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace StudentProjectManagement
+{
+    public partial class Student : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-82ANPR0U;Initial Catalog=ProjectDB;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+            conn.Open();
+        }
+
+        protected void Add_Click(object sender, EventArgs e)
+        {
+            int StudentID = 2;
+            string Sname = name.Text;
+            string SregNum = regNum.Text;
+            string Scontact = contact.Text;
+            string Sdegree = degree.Text;
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into Person(pname,contact,degree,regNum,CatId) values ('" + Sname + "','" + Scontact + "','" + Sdegree + "','" + SregNum + "','" + StudentID + "')";
+            cmd.ExecuteNonQuery();
+            reset();
+        }
+
+        protected void Update_Click(object sender, EventArgs e)
+        {
+            bool key1 = false, key2 = false, key3 = false;
+            string Sname = name.Text;
+            string SregNum = regNum.Text;
+            string Scontact = contact.Text;
+            string Sdegree = degree.Text;
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            if (SregNum != "") key1 = true;
+            if (Scontact != "") key2 = true;
+            if (Sdegree != "") key3 = true;
+
+            if (key1 && key2 && key3)
+            {
+                //key1 key2 key3
+                cmd.CommandText = "update Person set contact='" + Scontact + "',degree='" + Sdegree + "',regNum='" + SregNum + "' where pname='" + Sname + "' ";
+
+            }
+            else
+            {
+                if (key1)
+                {
+                    if (key2)
+                    {
+                        //key1 key2
+                        cmd.CommandText = "update Person set contact='" + Scontact + "',regNum='" + SregNum + "' where pname='" + Sname + "' ";
+                    }
+                    else if (key3)
+                    {
+                        //key1 key3
+                        cmd.CommandText = "update Person set degree='" + Sdegree + "',regNum='" + SregNum + "' where pname='" + Sname + "' ";
+                    }
+                    else
+                    {
+                        //key1 
+                        cmd.CommandText = "update Person set regNum='" + SregNum + "' where pname='" + Sname + "' ";
+
+                    }
+                }
+                else if (key2)
+                {
+                    if (key3)
+                    {
+                        //key2 key3
+                        cmd.CommandText = "update Person set contact='" + Scontact + "',degree='" + Sdegree + "' where pname='" + Sname + "' ";
+                    }
+                    else
+                    {
+                        //key2 
+                        cmd.CommandText = "update Person set contact='" + Scontact + "' where pname='" + Sname + "' ";
+                    }
+                }
+                else if (key3)
+                {
+                    //key3
+                    cmd.CommandText = "update Person set degree='" + Sdegree + "' where pname='" + Sname + "' ";
+                }
+
+            }
+
+
+            cmd.ExecuteNonQuery();
+           reset();
+        }
+
+        protected void Delete_Click(object sender, EventArgs e)
+        {
+            string Sname = name.Text;
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from Person where pname='" + Sname + "'";
+            cmd.ExecuteNonQuery();
+            reset();
+        }
+        protected void reset()
+        {
+            name.Text = "";
+            regNum.Text = "";
+            degree.Text = "";
+            contact.Text = "";
+        }
+    }
+}
