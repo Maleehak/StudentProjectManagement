3107d26f405cca3d430f50525a0bf50997fb5d10
Maleehak <39943018+Maleehak@users.noreply.github.com>
2019-12-16 10:51:04 +0500

    Add project files.

---
+++ StudentProjectManagement/Advisor.aspx
@@ -0,0 +1,104 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Advisor.aspx.cs" Inherits="StudentProjectManagement.Advisor" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>
+         University of Engineering and Techlogy ,Lahore
+    </title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link href="Style.css" rel="stylesheet"/>
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm fixed-top" id="customColor">
+  <div class="container">
+    <a class="navbar-brand" href="/">Student Project Management</a>
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav mr-auto">
+        <li class="nav-item"><a href="/" class="nav-link">Student</a></li>
+        <li class="nav-item"><a href="Advisor.aspx" class="nav-link">Advisor</a></li>
+        <li class="nav-item"><a href="/Home/Contact" class="nav-link">Project</a></li>
+          <li class="nav-item"><a href="/Home/Contact" class="nav-link">Group</a></li>
+         <li class="nav-item"><a href="/Home/Contact" class="nav-link">Evaluation</a></li>
+      </ul>
+
+     </div>
+      </div>
+    </nav>
+    <div class="container-fluid">
+        <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <div class="form-outer">
+                          <h1 class="text-center">Advisor</h1>
+         <form id="form1" runat="server">
+              <div class="input-container">
+        <div>
+            <table class="table-responsive">
+                <tr>
+                    <td>
+                        Name:
+                    </td>
+                     
+                    <td>
+                        <asp:TextBox ID="name" runat="server" Width="300px" Height="50px" ></asp:TextBox>
+                    </td>
+                </tr>
+                 <tr>
+                    <td>
+                        Rank:
+                    </td>
+                     
+                    <td>
+                        <asp:TextBox ID="rank" runat="server" Width="300px" Height="50px" ></asp:TextBox>
+                    </td>
+                </tr>
+                  <tr>
+                    <td>
+                        Contact:
+                    </td>
+                       
+                    <td>
+                        <asp:TextBox ID="contact" runat="server" Width="300px" Height="50px"></asp:TextBox>  
+                    </td>
+            
+                </tr>  
+                 
+                  <tr>
+                    <td>
+                        Designation:
+                    </td>
+                     
+                    <td>
+                        <asp:TextBox ID="designation" runat="server" Width="300px" Height="50px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                  
+                     <td>
+                     
+                    </td>
+                    <td>  
+                        <asp:Button ID="Add" runat="server" Text="Add" BackColor="#009999" Height="40" Width="90px" ForeColor="White" OnClick="Add_Click" />
+                          <asp:Button ID="Update" runat="server" Text="Update" BackColor="#009999" Height="40" Width="90px" ForeColor="White" OnClick="Update_Click" />
+                         <asp:Button ID="Delete" runat="server" Text="Delete"  BackColor="#009999" Height="40" Width="90px" ForeColor="White" OnClick="Delete_Click" />
+                    </td>
+                    <td>
+                       
+                   </td>
+                  
+                </tr>
+            </table>
+        </div>
+       </div>
+    </form>
+    </div>
+   </div>
+        </div>
+        </div>
+   
+</body>
+</html>
+
---
+++ StudentProjectManagement/Advisor.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+namespace StudentProjectManagement
+{
+    public partial class Advisor : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=HP-G3I5;Initial Catalog=ProjectDB;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+            conn.Open();
+
+        }
+
+        protected void Add_Click(object sender, EventArgs e)
+        {
+            int AdvisorID = 1;
+            string Aname = name.Text;
+            string Arank = rank.Text;
+            string Acontact = contact.Text;
+            string Adesignation = designation.Text;
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into Person(pname,contact,designation,rank,CatId) values ('" + Aname + "','" + Acontact + "','" + Adesignation + "','" + Arank + "','" + AdvisorID + "')";
+            cmd.ExecuteNonQuery();
+            reset();
+
+
+        }
+
+        protected void Update_Click(object sender, EventArgs e)
+        {
+            bool key1 = false, key2 = false, key3 = false;
+            string Aname = name.Text;
+            string Arank = rank.Text;
+            string Acontact = contact.Text;
+            string Adesignation = designation.Text;
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            if (Arank != "") key1 = true;
+            if (Acontact != "") key2 = true;
+            if (Adesignation != "") key3 = true;
+
+            if (key1 && key2 && key3)
+            {
+                //key1 key2 key3
+                cmd.CommandText = "update Person set contact='" + Acontact + "',designation='" + Adesignation + "',rank='" + Arank + "' where pname='" + Aname + "' ";
+
+            }
+            else
+            {
+                if (key1)
+                {
+                    if (key2)
+                    {
+                        //key1 key2
+                        cmd.CommandText = "update Person set contact='" + Acontact + "',rank='" + Arank + "' where pname='" + Aname + "' ";
+                    }
+                    else if (key3)
+                    {
+                        //key1 key3
+                        cmd.CommandText = "update Person set designation='" + Adesignation + "',rank='" + Arank + "' where pname='" + Aname + "' ";
+                    }
+                    else
+                    {
+                        //key1 
+                        cmd.CommandText = "update Person set rank='" + Arank + "' where pname='" + Aname + "' ";
+
+                    }
+                }
+                else if (key2)
+                {
+                    if (key3)
+                    {
+                        //key2 key3
+                        cmd.CommandText = "update Person set contact='" + Acontact + "',designation='" + Adesignation + "' where pname='" + Aname + "' ";
+                    }
+                    else
+                    {
+                        //key2 
+                        cmd.CommandText = "update Person set contact='" + Acontact + "' where pname='" + Aname + "' ";
+                    }
+                }
+                else if (key3)
+                {
+                    //key3
+                    cmd.CommandText = "update Person set designation='" + Adesignation + "' where pname='" + Aname + "' ";
+                }
+
+            }
+
+
+            cmd.ExecuteNonQuery();
+            reset();
+
+        }
+
+        protected void Delete_Click(object sender, EventArgs e)
+        {
+            string Aname = name.Text;
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from Person where pname='" + Aname + "'";
+            cmd.ExecuteNonQuery();
+            reset();
+        }
+        protected void reset()
+        {
+            name.Text = "";
+            rank.Text = "";
+            designation.Text = "";
+            contact.Text = "";
+        }
+        public void displayData()
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Person";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            //GridView1.DataSource = dt;
+            //GridView1.DataBind();
+        }
+    }
+}
